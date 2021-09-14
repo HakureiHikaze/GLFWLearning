401b1e821e42b065cdd76ab6efd49a63f8553e2b
HakureiHikaze <wgyaaa123@gmail.com>
2021-09-14 09:24:27 +0800

    dev

---
+++ DrawTest01.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Hikaze on 2021/9/14 8:42.
+// 
+//
+
+#pragma once
+
+#include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "Util.hpp"
+
+void DrawTriangleAndRectangle(void(*)(GLFWwindow*),GLFWframebuffersizefun);
